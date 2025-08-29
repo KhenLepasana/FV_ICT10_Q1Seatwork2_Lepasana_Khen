ddca5c9363ad65adda6880dec056bdb8b43ee393
KhenLepasana <Lepasana.kic@obmontessori.edu.ph>
2025-08-29 08:56:40 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from pyscript import display
+
+# Variables
+restaurant_name = "The Goodfather" #string
+owner_name = "Khen Lepasana" #string
+year_established = 1939 #integer
+popular_item_price = 320.00 #float
+has_delivery = True #boolean
+product_names = ["Truffle Pasta", "Cheese Pizza", "Caesar Salad"] #list
+business_hours = "10:00 AM - 9:00 PM" #string
+menu_prices = {
+    "Truffle Pasta": 320.00,
+    "Cheese Pizza": 250.00,
+    "Caesar Salad": 150.00,
+    "Iced Latte": 80.00,
+    "Fresh Lemonade": 60.00
+} #dictionary
+common_allergens = ["Nuts", "Dairy", "Gluten"] #list
+tax_rate = 0.09 #float
+
+#display(restaurant_name)
+#display(owner_name)
+#display(year_established)
+#display(popular_item_price)
+#display(has_delivery)
+#display(product_names)
+#display(business_hours)
+#display(menu_prices)
+#display(common_allergens)
+#display(tax_rate)
